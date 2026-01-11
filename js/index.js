b0e4ed469828ac93a32b4fa91ae64fc4d4f705a8
Dan Collins <dan.collins.dev@gmail.com>
2026-01-10 22:12:18 -0500

    updated demo

---
+++ js/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const elements = {
+    parent: document.getElementById("parent"),
+    child: document.getElementById("child"),
+    grandChild: document.getElementById("grand-child"),
+    sibling: document.getElementById("sibling"),
+}
+
+elements.parent.addEventListener("click", function (event) {
+    alert(`Current element handling event: ${this.dataset.nodeName}\nEvent emitted By: ${event.target.dataset.nodeName}`);
+});
+
+elements.child.addEventListener("click", function (event) {
+    alert(`Current element handling event: ${this.dataset.nodeName}\nEvent emitted By: ${event.target.dataset.nodeName}`);
+});
+
+elements.grandChild.addEventListener("click", function (event) {
+    alert(`Current element handling event: ${this.dataset.nodeName}\nEvent emitted By: ${event.target.dataset.nodeName}`);
+});
+
+elements.sibling.addEventListener("click", function (event) {
+    alert(`Current element handling event: ${this.dataset.nodeName}\nEvent emitted By: ${event.target.dataset.nodeName}`);
+});
